822ec6766f8a9f4dbe0833e3dce1c69381a0b887
lukdwo <79221808+lukdwo@users.noreply.github.com>
2022-12-29 21:01:18 +0100

    Basic AD cert script

---
+++ c_certs/cert_generate.ps1
@@ -0,0 +1,45 @@
+﻿if (Get-ChildItem -Path Cert:\CurrentUser\My\*) {
+        
+        # If user has cert, give a warning
+        Write-Warning "User has some cert on this PC"
+    }
+    else {
+        write-host $env:username
+        write-host $args[0]
+$cert_password = $args[0]
+$CertName = $env:username
+$CSRPath = "c:\certs\$($CertName).csr"
+$INFPath = "c:\certs\$($CertName).inf"
+$CERPath = "c:\certs\$($CertName).cer"
+$PFXPath = "c:\certs\$($CertName).pfx"
+
+
+Write-Host "Creating CertificateRequest(CSR) for $CertName `r "
+
+
+$Signature = '$Windows NT$' 
+$INF =
+@"
+[Version]
+Signature= "$Signature" 
+[NewRequest]
+Subject = "CN=$CertName,OU=Users,OU=Users,OU=Own,DC=domain,DC=com"
+Exportable = TRUE
+[RequestAttributes]
+CertificateTemplate=Temp_name
+"@
+write-Host "Certificate Request is being generated `r "
+$INF | out-file -filepath $INFPath -force
+certreq -new $INFPath $CSRPath 
+certreq -attrib "CertificateTemplate:Temp_name" -submit -config "ca.domain.com\ca" $CSRPath $CERPath $PFXPath 
+certreq -accept $CERPath
+
+
+#Export Cer
+$mycrtpwd = ConvertTo-SecureString -String $cert_password -Force -AsPlainText
+Get-ChildItem -Path Cert:\CurrentUser\My\* | Export-PfxCertificate -FilePath $PFXPath -Password $mycrtpwd
+
+          # If cert is created, show message.
+        Write-Host "The user cert has been generated" -ForegroundColor Cyan
+    }
+ 
---
+++ cert_from_file.ps1
@@ -0,0 +1,27 @@
+﻿# Store the data from MOCK.csv in the $ADUsers variable
+$ADUsers = Import-Csv C:\certs\MOCK.csv -Delimiter ","
+
+# Loop through each row containing user details in the CSV file
+foreach ($User in $ADUsers) {
+
+    #Read user data from each field in each row and assign the data to a variable as below
+    $username = $User.username
+    $ad_username = "domain\$username"
+    $firstname = $User.firstname
+    $lastname = $User.lastname
+    $ad_password = $User.ad_password
+	$cert_password = $User.cert_password
+    Write-Host $ad_username
+    Write-Host $ad_password
+
+      $securePassword = ConvertTo-SecureString -String $ad_password -Force -AsPlainText
+      $credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $ad_username, $securePassword
+      $args="-noprofile -file c:\certs\cert_generate.ps1 $cert_password"
+      Write-Host $args
+      Start-Process powershell.exe -Credential $credential -ArgumentList $args
+}
+
+Read-Host -Prompt "Press Enter to exit"
+   
+   
+    
